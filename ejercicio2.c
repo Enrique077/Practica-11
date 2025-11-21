b071424ae2649af658b53c48a6233236f1dc3c5d
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 19:51:51 -0600

    ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,15 @@
+/*Enrique Mena Hernández
+programa 2*/
+
+#include <stdio.h>
+void sumar(); // prototipo de la función 
+int main()
+{
+ sumar(); // llamado de la función suma
+}
+void sumar() // función suma
+{
+ int z, x=5, y=10; //variables locales
+ z=x+y;
+ printf("%i",z);
+}
