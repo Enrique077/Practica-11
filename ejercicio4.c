8a517428c6d1efc58827a8aa2a0d600db84e6868
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 19:55:28 -0600

    ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,26 @@
+/*Enrique Mena Hernández
+programa 4*/
+
+#include<stdio.h>
+void incremento();
+/* La variable enteraGlobal es vista por todas
+ las funciones (main e incremento) */
+int enteraGlobal;
+int main()
+{
+ // La variable cont es local a la función main
+ int cont;
+ enteraGlobal = 0; // La función main accede a la variable global
+ for (cont=0 ; cont<5 ; cont++) 
+ {
+   incremento();
+ } 
+ return 0;
+}
+void incremento()
+{
+ // La variable enteraLocal es local a la función incremento
+ int enteraLocal = 5;
+ enteraGlobal += 2;
+ printf("global(%i) + local(%i) = %d\n",enteraGlobal, enteraLocal, enteraGlobal+enteraLocal);
+}
