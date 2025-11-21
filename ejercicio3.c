ff6a1efdd4431171f267ed051c206d1aeb42fbf5
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 19:52:56 -0600

    ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,16 @@
+/*Enrique Mena Hernández
+programa 3*/
+
+#include <stdio.h>
+int resultado; //variable global
+void multiplicar(); // prototipo de la función 
+int main()
+{
+ multiplicar(); //llamado de la función multiplicar
+ printf("%i",resultado);
+ return 0;
+}
+void multiplicar() //función multiplicar
+{
+ resultado = 5 * 4;
+}
